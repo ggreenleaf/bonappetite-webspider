24e8b252e4bf4c3ed2ff1e10022201d06ae4dcf5
ggreenleaf <geoffrey.greenleaf@gmail.com>
2015-10-29 14:46:46 -0500

    simple single page crawl

--- bonappetit/spiders/bonappetitspider.py
+++ bonappetit/spiders/bonappetitspider.py
@@ -1,5 +1,5 @@
-from scrapy.spider import BaseSpider
-from scrapy.select import HtmlXPathSelector
+from scrapy.spiders import Spider
+from scrapy.selector import Selector
 from bonappetit.items import BonappetitItem
 
 #  'title'   : response.xpath('//*[@id="recipe-ingredients"]/div[2]/div[1]/h3/text()').extract(),
@@ -7,12 +7,21 @@ from bonappetit.items import BonappetitItem
 #           'prep' : response.xpath('//*[@id="recipe-ingredients"]/div[2]/div[3]/div[2]/div/ul/li[1]/div/text()').extract()
 #      
 
-class RecipeeSpider(BaseSpider):
-	name = "BonAppetit"
+class RecipeeSpider(Spider):
+	name = "bonappetit"
 	allowed_domains = ["bonappetit.com"]
 	# start_urls = ["http://http://www.bonappetit.com/recipes/"]
-	start_urls = ['http://www.bonappetit.com/recipe/passover-chocolate-toffee-matzo']
-	items = []
+	start_urls = ["http://www.bonappetit.com/recipe/passover-chocolate-toffee-matzo"]
+
 	def parse(self, response):
-		hxs = HtmlXPathSelector(response)
+		hxs = Selector(response)
+		print response
+		item = BonappetitItem()
+		items = []
+		item["title"] = response.url
+		item["prep"] = hxs.xpath('//*[@id="recipe-ingredients"]/div[2]/div[3]/div[2]/div/ul/li[1]/div/text()').extract()		
+		item["ingredients"] = hxs.xpath('//*[@id="recipe-ingredients"]/div[2]/div[2]/div[1]/div/ul/li/span/span[3]/text()').extract()
+		items.append(item)
+		return item
 		
+	
